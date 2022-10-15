e30783077e9bd89bed04578d0d43c50d074fd2ad
Krayan321 <krayan321@gmail.com>
2022-10-15 12:12:16 +0200

    Adult i Child

---
+++ demo/src/main/java/Adult.java
@@ -0,0 +1,21 @@
+public class Adult extends ClientType {
+    @Override
+    public int getMaxDays() {
+        return 1440;
+    }
+
+    @Override
+    public int getMaxBooks() {
+        return 5;
+    }
+
+    @Override
+    public float getPenalty() {
+        return super.getPenalty()*2.0f;
+    }
+
+    @Override
+    public String getTypeInfo() {
+        return "ADULT";
+    }
+}
---
+++ demo/src/main/java/Child.java
@@ -0,0 +1,21 @@
+public class Child extends ClientType{
+    @Override
+    public int getMaxDays() {
+        return 2160;
+    }
+
+    @Override
+    public int getMaxBooks() {
+        return 3;
+    }
+
+    @Override
+    public String getTypeInfo() {
+        return "CHILD";
+    }
+
+    @Override
+    public float getPenalty() {
+        return super.getPenalty() * 1.2f;
+    }
+}
