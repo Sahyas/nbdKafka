0dbe690edd765781ad059d2f65145a8f690afe97
Krayan321 <krayan321@gmail.com>
2022-10-18 18:50:45 +0200

    Book jako entity

--- src/main/java/com/nbd/model/Book.java
+++ src/main/java/com/nbd/model/Book.java
@@ -1,15 +1,33 @@
 package com.nbd.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "book")
+@Access(AccessType.FIELD)
+@ToString
+
 public class Book {
+    @Id
+    @Column(name = "book_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "title")
+    @NotNull
     private String title;
+    @Column(name = "author")
+    @NotNull
     private String author;
+    @Column(name = "serialNumber")
+    @NotNull
     private String serialNumber;
+    @Column(name = "genre")
     private String genre;
     boolean IsArchive = false;
 
